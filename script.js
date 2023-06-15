833788cb72b16b2fa2879373e5da9c8bada23143
WesleyMut8 <wesley@mut.log.br>
2023-06-15 04:24:52 -0300

    Initial Commit:
    Commit with all the first version.

---
+++ script.js
@@ -0,0 +1,11 @@
+let toggleMode = () => {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // base
+  // if (html.classList.contains("light")) {
+  //   html.classList.remove("light")
+  // } else {
+  //   html.classList.add("light")
+  // }
+}
